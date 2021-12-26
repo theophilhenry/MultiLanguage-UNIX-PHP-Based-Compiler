6b4b466afa2e04af48031ab1ce8b51368cff124c
theophilhenry <s160419078@student.ubaya.ac.id>
2021-12-26 21:48:13 +0800

    try

--- index_cpp.php
+++ index_cpp.php
@@ -1,29 +1,22 @@
 <?php
-//proses
-$lang = "cpp";
-$command1 = "gcc programPeserta.cpp -o compilePeserta"; // for compile
-$command2 = "time compilePeserta <inputSoal.in> outputPemain.out";
-
-echo "Com1 : " . $command1 . "<br>";
-echo "Com2 : " . $command2 . "<br>";
-
 $result = "Accepted";
 $runtime = 0;
+
+//proses
+$lang = "cpp";
+$command1 = "clang submission/cpp.cpp -o temp/compiled"; // for compile
+$command2 = "time temp/compiled <soal/cpp.in> temp/result.out";
 
 $descriptorspec = array(
     0 => array("pipe", "r"), // stdin is a pipe that the child will read from
     1 => array("pipe", "w"), // stdout is a pipe that the child will write to
     2 => array("pipe", "w") //stderr is a pipe that the child will write to
 );
-echo "Descriptor:";
-print_r($descriptorspec);
-echo "<br/>";
 
-$cwd = '/var/www/html/compiler/theo/asset'; //The initial working dir for the command
-$process = proc_open($command1, $descriptorspec, $pipes, $cwd);
+$process = proc_open($command1, $descriptorspec, $pipes);
+
 
 if (is_resource($process)) {
-
     $out = stream_get_contents($pipes[1]);
     echo "1." . $out . "<br />";
     fclose($pipes[1]);
@@ -32,9 +25,7 @@ if (is_resource($process)) {
     echo "2." . $out . "<br />";
     fclose($pipes[2]);
 
-    $return_value = proc_close($process);
-    if ($return_value != 0)
-        $result = 'Compile Error';
+    if (proc_close($process) != 0) $result = 'Compile Error';
 }
 
 
@@ -43,14 +34,13 @@ if ($result == "Accepted") {
     $memory_limit = 64 * 1024; //64MB
     $time_limit = 30; //15second
 
-    $process = proc_open('bash -c "ulimit -St ' . $time_limit . ' -Sm ' . $memory_limit . '; ' . $command2 . '"', $descriptorspec, $pipes, $cwd);
-    // $process = proc_open($command2, $descriptorspec, $pipes, $cwd);
-    echo "<br />";
+    $process = proc_open("bash -c 'ulimit -St $time_limit -Sm $memory_limit ; $command2'", $descriptorspec, $pipes);
+    
     if (is_resource($process)) {
         $stream = stream_get_contents($pipes[2]);
         echo "Stream : " . $stream;
-
         fclose($pipes[2]);
+
         $return_value = proc_close($process);
 
         $timelimitstring = "CPU time limit exceeded";
@@ -77,10 +67,9 @@ if ($result == "Accepted") {
     }
 }
 
-
 //jika tetap masih AC(uda lewat TL), harus dicek sama tidak dengan output yang diinginkan
 if ($result == "Accepted") {
-    $process = proc_open('cmp outputPemain.out outputSoal.out', $descriptorspec, $pipes, $cwd);
+    $process = proc_open('cmp temp/result.out soal/cpp.out', $descriptorspec, $pipes);
 
     if (is_resource($process)) {
         $return_value = proc_close($process);
---
+++ submission/java.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soal.soalilpc;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Testing {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //misalkan aku suka programming maka aku mau ikut lomba programming di UBAYA yaitu ILPC 2022
+        //3 4 11 4 3 3 4 5 11 2 5 5 4 4
+        //E O I O E E O A I U A A O O
+        ArrayList<String> listOfJawaban = new ArrayList<String>();
+        int banyakInput = Integer.parseInt(in.nextLine());
+        while (banyakInput != 0) {
+            banyakInput = banyakInput - 1;
+            String kalimat = in.nextLine();
+            String[] tiapKata = kalimat.split(" ");
+            ArrayList<Character> murniKata = new ArrayList<>();
+            String passwordKu = "";
+            for (int i = 0; i < tiapKata.length; i++) {
+                for (char ch : tiapKata[i].toCharArray()) {
+                    if (Character.isLetter(ch)) {
+                        murniKata.add(ch);
+                    }
+                }
+                if (murniKata.size() % 5 == 0) {
+                    passwordKu += "A";
+                } else if (murniKata.size() % 5 == 1) {
+                    passwordKu += "I";
+                } else if (murniKata.size() % 5 == 2) {
+                    passwordKu += "U";
+                } else if (murniKata.size() % 5 == 3) {
+                    passwordKu += "E";
+                } else {
+                    passwordKu += "O";
+                }
+                for (char ch : tiapKata[i].toCharArray()) {
+                    if (!Character.isLetter(ch)) {
+                        passwordKu += ch;
+                    }
+                }
+                murniKata.clear();
+            }
+            listOfJawaban.add(passwordKu);
+        }
+        for (String jawaban : listOfJawaban)
+        {
+            System.out.println(jawaban);
+        }
+
+    }
+}
